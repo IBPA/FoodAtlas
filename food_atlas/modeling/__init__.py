43c312922c2a9ada50fa5456ca2bf2efae1fa483
Fangzhou Li <fzli0805@gmail.com>
2022-09-08 15:50:32 -0700

    refactored

--- food_atlas/modeling/__init__.py
+++ food_atlas/modeling/__init__.py
@@ -1,9 +1,11 @@
+from ._model import load_model
 from ._train import train_nli_model
-from ._dataset import get_food_atlas_data_loader
-from ._evaluate import get_all_metrics
+from ._dataset import get_food_atlas_data_loaders
+from ._metrics import get_all_metrics
 
 __all__ = [
+    'load_model',
     'train_nli_model',
-    'get_food_atlas_data_loader',
+    'get_food_atlas_data_loaders',
     'get_all_metrics',
 ]
