15186ffb4c3df6c341229e835bb1d1a3e68a4480
Fangzhou Li <fzli0805@gmail.com>
2022-10-04 11:49:56 -0700

    refactored script bash files

---
+++ scripts/run_unlabeled_data_prediction.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=FA_predict
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=fzli@ucdavis.edu
+#SBATCH --output=/home/lfz/git/FoodAtlas/logs/%j.out
+#SBATCH --error=/home/lfz/git/FoodAtlas/logs/%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32
+#SBATCH --time=48:00:00
+
+cd ..
+
+CUDA_VISIBLE_DEVICES=0, python -m food_atlas.entailment.run_unlabeled_data_prediction \
+    outputs/data_generation/to_predict_1.tsv \
+    biobert \
+    outputs/entailment_model/1 \
+    --path-output-data-to-predict tests/to_predict_1_predicted_2.tsv \
+
+cd scripts
