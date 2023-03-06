91d21efb2129d24675c9ed0cb7c8a8195824acdf
Jason Youn <jyoun@ucdavis.edu>
2023-03-06 12:18:26 -0800

    Removed unused files

--- food_atlas/kgc/optimize_hp_pykeen_4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-CUDA_VISIBLE_DEVICES=2 python run_hpo.py \
-    --train_dir=../../outputs/kgc/data/annotations_predictions_extdb_mesh_ncbi \
-    --val_test_dir=../../outputs/kgc/data \
-    --output_dir=../../outputs/kgc/pykeen/annotations_predictions_extdb_mesh_ncbi/hpo \
-    --models=ER-MLP,ComplEx
