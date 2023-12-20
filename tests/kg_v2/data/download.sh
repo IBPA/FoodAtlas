fe56345700774e0103afeb11889494eaa6fec516
Fangzhou Li <fzli0805@gmail.com>
2023-12-20 12:30:08 -0800

    add wip files

---
+++ tests/kg_v2/data/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download the data from USDA FDC.
+https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_foundation_food_csv_2023-10-26.zip
+https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_sr_legacy_food_csv_2018-04.zip
--- tests/kg_v2/get_fdc_food_table.py
+++ tests/kg_v2/get_fdc_food_table.py
@@ -1,11 +1,5 @@
 import pandas as pd
 
-ATTR_NAMES = [
-    'FoodOn Ontology name for FDC item',
-    'FoodOn Ontology ID for FDC item',
-    'NCBI Taxon',
-]
-
 
 if __name__ == '__main__':
     food_categories = pd.read_csv(
@@ -41,8 +35,16 @@ if __name__ == '__main__':
             attr = attr.set_index('name')
 
             for attr_name, col_name in zip(
-                ATTR_NAMES,
-                ['foodon_name', 'foodon_url', 'ncbi_taxon_url']
+                [
+                    'FoodOn Ontology name for FDC item',
+                    'FoodOn Ontology ID for FDC item',
+                    'NCBI Taxon',
+                ],
+                [
+                    'foodon_name',
+                    'foodon_url',
+                    'ncbi_taxon_url',
+                ]
             ):
                 result[col_name] = attr.loc[attr_name, 'value'] \
                     if attr_name in attr.index else None
@@ -67,6 +69,9 @@ if __name__ == '__main__':
         "tests/kg_v2/data/FDC/FoodData_Central_sr_legacy_food_csv_2018-04/"
         "food_attribute_type.csv",
     )
+    print(data_sr)
+    print(data_sr_attr)
+    print(data_sr_attr_id)
 
     def _extract_sr_food_attributes(row):
         result = {
@@ -77,35 +82,27 @@ if __name__ == '__main__':
         }
         if row['fdc_id'] in data_sr_attr.index:
             attr = data_sr_attr.loc[row['fdc_id']]
-            print(attr)
-        #     if type(attr) == pd.Series:
-        #         attr = pd.DataFrame(attr).T
-        #     attr = attr.set_index('name')
+            if type(attr) == pd.Series:
+                attr = pd.DataFrame(attr).T
+            attr = attr.set_index('name')
 
-        #     for attr_name, col_name in zip(
-        #         ATTR_NAMES,
-        #         ['foodon_name', 'foodon_url', 'ncbi_taxon_url']
-        #     ):
-        #         result[col_name] = attr.loc[attr_name, 'value'] \
-        #             if attr_name in attr.index else None
+            result['common_names'] = attr['value'].tolist()
 
-        # return pd.Series(result)
+        return pd.Series(result)
 
-    print(data_sr.apply(_extract_sr_food_attributes, axis=1))
-    # print(data_sr)
-    # print(data_sr_attr)
-    # print(data_sr_attr_id)
-    # # Construct food table.
-    # data = pd.concat(
-    #     [data_ff, data_sr], ignore_index=True
-    # ).drop(columns=['publication_date'])
-    # data['food_category'] = data['food_category_id'].apply(
-    #     lambda x: food_categories.loc[x, 'description'] \
-    #         if x in food_categories.index else None
-    # )
+    data_sr = pd.concat(
+        [data_sr, data_sr.apply(_extract_sr_food_attributes, axis=1)],
+        axis=1
+    )
 
-    # print(data)
+    # Construct food table.
+    data = pd.concat(
+        [data_ff, data_sr], ignore_index=True
+    ).drop(columns=['publication_date'])
+    data['food_category'] = data['food_category_id'].apply(
+        lambda x: food_categories.loc[x, 'description'] \
+            if x in food_categories.index else None
+    )
 
-
-
-    pass
+    print(data)
+    data.to_csv('test/kg_v2/outputs/check.csv')
